448287fe3cd4cdefa21ac2150bf2dd96c23e34d0
Aayush Sinha <aayush.aryan@me.com>
2021-08-06 19:50:17 +0530

    Make compatible with Firefox.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 //Configuration Parameters
 let canvas      = document.querySelector("#main_canvas");
-let ww          = canvas.width = window.innerWidth;
-let wh          = canvas.height = window.innerHeight;
+let ww          = window.innerWidth;
+let wh          = window.innerHeight;
+let slowBrowser = false;
+let isTested    = false;
+let scaleToFit  = 3;
 let ctx         = canvas.getContext("2d");
 let particles   = [];
 let amount      = 0;
 let mouse       = {x: -9999, y: -9999};
-let radius      = 150;
 let colors      = ["#695aa6","#444444", "#9370DB","#BA55D3"];
 let text        = "< Aayush Sinha />";
 
+let radius      = 150;
 //Conditions for Responsiveness
 if(ww <= 320)       { radius = 15;  }
 else if(ww <= 375)  { radius = 20;  } 
@@ -21,8 +24,8 @@ else if(ww <= 2048) { radius = 100; }
 //Main Particle Class
 class Particle {
     constructor(x, y) {
-        this.x    = Math.random() * ww;
-        this.y    = Math.random() * wh;
+        this.x    = Math.random() * canvas.width;
+        this.y    = Math.random() * canvas.height;
         this.vx   = (Math.random() - 0.5) * 10;
         this.vy   = (Math.random() - 0.5) * 10;
         let rand  = Math.random();
@@ -39,17 +42,36 @@ class Particle {
             this.r = (rand * 4) + 6;
         }
 
+        if(slowBrowser) {
+            if (ww <= 2048) {
+                this.r = 1;
+            } else if(ww <= 3000) {
+                this.r = (rand * 1) + 2;
+            } else {
+                this.r = (rand * 2) + 2;
+            }
+            
+        }
+        
+
         this.accX = 0;
         this.accY = 0;
         this.friction = Math.random() * 0.05 + 0.9;
+        if(slowBrowser) {
+            this.friction /= 1.1;
+        }
 
-        this.color = colors[Math.floor(Math.random() * 5)];
+        this.color = colors[Math.floor(Math.random() * 4)];
     }
 
     //Rended method
     render() {
         this.accX = (this.dest.x - this.x) / 100;
         this.accY = (this.dest.y - this.y) / 100;
+        if(slowBrowser) {
+            this.accX *= 8;
+            this.accY *= 8;
+        }
         this.vx  += this.accX;
         this.vy  += this.accY;
         this.vx  *= this.friction;
@@ -59,7 +81,7 @@ class Particle {
 
         ctx.fillStyle = this.color;
         ctx.beginPath();
-        ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
+        ctx.arc(Math.floor(this.x), Math.floor(this.y), Math.floor(this.r), Math.PI * 2, false);
         ctx.fill();
 
         //Exploding logic on Mouse Move
@@ -69,6 +91,10 @@ class Particle {
         if (distance  < (radius)) {
             this.accX = (this.x - mouse.x) / 10;
             this.accY = (this.y - mouse.y) / 10;
+            if(slowBrowser) {
+                this.accX *= 4;
+                this.accY *= 4;
+            }
             this.vx  += this.accX;
             this.vy  += this.accY;
         }
@@ -76,10 +102,15 @@ class Particle {
     }
 }
 
+
 //Change mouse pointers on mouse movement
 function onMouseMove(e){
     mouse.x = e.clientX;
     mouse.y = e.clientY;
+    if(slowBrowser) {
+        mouse.x /= scaleToFit;
+        mouse.y /= scaleToFit;
+    }
 }
 
 //Handling for touch inputs. Take only first touch. (For multi touch devices)
@@ -87,6 +118,10 @@ function onTouchMove(e){
     if(e.touches.length > 0 ){
         mouse.x = e.touches[0].clientX;
         mouse.y = e.touches[0].clientY;
+        if(slowBrowser) {
+            mouse.x /= scaleToFit;
+            mouse.y /= scaleToFit;
+        }
     }
 }
 
@@ -98,27 +133,34 @@ function onTouchEnd(e){
 
 //Initialization Function
 function initScene(){
-    ww = canvas.width = window.innerWidth;
-    wh = canvas.height = window.innerHeight;
+    ww = window.innerWidth;
+    wh = window.innerHeight;
+    if(slowBrowser) {
+        canvas.width = ww/scaleToFit;
+        canvas.height = wh/scaleToFit;
+    } else {
+        canvas.width = ww;
+        canvas.height = wh;
+    }    
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     //Fill canas with text (Temporarily!)
-    ctx.font = "bold "+(ww/10)+"px sans-serif";
+    ctx.font = "bold "+(canvas.width/10)+"px sans-serif";
     ctx.textAlign = "center";
-    ctx.fillText(text, ww/2, wh/2);
+    ctx.fillText(text, canvas.width/2, canvas.height/2);
 
     //Take snapshot and clear canvas
-    let data  = ctx.getImageData(0, 0, ww, wh).data;
+    let data  = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.globalCompositeOperation = "screen";
 
     //Let the fun games begin!
     particles = [];
-    for(var i=0;i<ww;i+=Math.round(ww/150)){
-        for(var j=0;j<wh;j+=Math.round(ww/150)){
-            if(data[ ((i + j*ww)*4) + 3] > 150){
-                particles.push(new Particle(i,j));
+    for(var i=0;i<canvas.width;i+=Math.round(canvas.width/150)){
+        for(var j=0;j<wh;j+=Math.round(canvas.width/150)){
+            if(data[ ((i + j*canvas.width)*4) + 3] > 150){
+                    particles.push(new Particle(i,j));
             }
         }
     }
@@ -128,11 +170,57 @@ function initScene(){
 //Event Looooooooooooop
 function render(a) {
     requestAnimationFrame(render);
+
+    let t0 = 0;
+    if(!isTested) {
+        t0 = performance.now();
+    }
+    
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (var i = 0; i < amount; i++) {
         particles[i].render();
     }
+
+    let t1 = 0;
+    if(!isTested) {
+        t1 = performance.now();
+        if(t1-t0>5) {
+            slowBrowser = true;
+            browserIsSlow();
+        }
+        isTested = true;
+    }
+        
+    
 };
+
+function browserIsSlow() {
+    if(slowBrowser) {
+        if(ww <= 320) {
+            scaleToFit = 1;
+        }
+        canvas.width                    = ww/scaleToFit;
+        canvas.height                   = wh/scaleToFit;
+        canvas.style.transformOrigin    = '0 0'; //Scale from the Top left corner
+        canvas.style.transform          = 'scale(' + scaleToFit + ')';
+        radius                          = 170/(2*scaleToFit);
+        ctx                             = canvas.getContext("2d");
+        particles                       = [];
+        amount                          = 0;
+
+        //Conditions for Responsiveness
+        if(ww <= 320)       { radius = 25/(scaleToFit*2);  }
+        else if(ww <= 375)  { radius = 20/(scaleToFit*2);  } 
+        else if(ww <= 768)  { radius = 60/(scaleToFit*2);  } 
+        else if(ww <= 1024) { radius = 80/(scaleToFit*2);  }  
+        else if(ww <= 1440) { radius = 100/(scaleToFit*2); }
+        else if(ww <= 2048) { radius = 140/(scaleToFit*2); }
+
+        document.getElementById('slowBrowserMessage').style.display = "initial";
+
+        initScene();
+    }
+}
 
 //Initialize and Render
 window.addEventListener("resize",    initScene);
@@ -198,8 +286,8 @@ constructor(collegeName, degree, dates) {
 }
 }
 console.log("%cHi👋🏻", "font-size: 48px;");
-console.log("%cHice to meet you in this dark alley.", "font-size: 24px;");
-console.log('');
+console.log("%cHice to meet you in this back alley.", "font-size: 24px;");
+console.log('\n');
 console.log('%c👇🏻 Profile', 'font-size: 24px');
 
 console.dir(new workExperience());
@@ -221,7 +309,7 @@ let edu = [];
   console.log('\n%cTech Stack\n\n%cDSA%c  %cJava%c  %cPHP%c  %cNodeJS%c  %cHTML/CSS/JS%c  %cReactJS + VueJS%c  %cAndroid%c  %cMongoDB%c  %cFirebase%c  %cAWS + GCP%c  %cPostgreSQL%c  %cmySQL + SQLite', 'font-size: 30px; color: green', 
   tagStyle, noStyle, tagStyle, noStyle, tagStyle, noStyle, tagStyle, noStyle, tagStyle, noStyle, tagStyle, noStyle, tagStyle, noStyle, tagStyle, noStyle, tagStyle, noStyle, tagStyle, noStyle, tagStyle, noStyle, tagStyle);
 
-  console.log('');
+  console.log('\n');
   console.log('%c(Note: There is an image below which might not work in non-modern browsers)', 'font-size: 16px; color: orange');
   console.image("https://raw.githubusercontent.com/aayusharyan/yush.dev/main/not_sure.jpg");
 
